b15109ad2944f1d287280dace3c4786a5271e32a
Young-Kyoon Suh <yksuh@cs.arizona.edu>
2017-03-03 08:51:43 +0900

    2015's run summary

--- pt_characterization/new_data/empv5_et_hist.r
+++ pt_characterization/new_data/empv5_et_hist.r
@@ -287,11 +287,12 @@ sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""),
 xlab='ET (ms)', ylab=expression('Frequency'))
 dev.off()
 
+#8192 sec
+
 x = read.csv(file="8192_sec.dat",head=TRUE,sep="\t")
 setEPS()
 postscript("8192_sec_et_hist1_v5.eps")
-#x <- subset(x, x$ITERNUM <= 40) 
-x <- subset(x, x$ITERNUM != 84 & x$ITERNUM != 95 & x$ITERNUM != 105 & x$ITERNUM != 116 & x$ITERNUM != 45 & x$ITERNUM != 56 & x$ITERNUM != 66 & x$ITERNUM != 77)
+x <- subset(x, x$ITERNUM <= 40) 
 x_up = mean(x$METIME) + 2*sd(x$METIME)
 x_dn = mean(x$METIME) - 2*sd(x$METIME)
 binsize=50
@@ -303,16 +304,18 @@ xmax <-max(x$METIME)
 ymax <- max(h$counts)
 ymax <- ceiling(ymax/100)*100
 ymax <- max(h$counts)
-ymax <- 50
+ymax <- 8
 plot(h, freq=TRUE,ylim=c(0,ymax), xlim=c(xmin-200,xmax+200),col="blue", main='ET frequency on INC8192', 
 sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), 
 xlab='ET (ms)', ylab=expression('Frequency'))
 dev.off()
 
+
+### 16384 sec
 x = read.csv(file="16384_sec.dat",head=TRUE,sep="\t")
 x <- subset(x, x$ITERNUM <= 40)
 setEPS()
-postscript("16384_sec_et_hist_v5.eps")
+postscript("16384_sec_et_hist1_v5.eps")
 x <- subset(x, x$ITERNUM != 10 & x$ITERNUM != 16)
 x_up = mean(x$METIME) + 2*sd(x$METIME)
 x_dn = mean(x$METIME) - 2*sd(x$METIME)
@@ -331,23 +334,4 @@ sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""),
 xlab='ET (ms)', ylab=expression('Frequency'))
 dev.off()
 
-x = read.csv(file="16384_sec.dat",head=TRUE,sep="\t")
-setEPS()
-postscript("16384_sec_et_hist1_v5.eps")
-x <- subset(x, x$ITERNUM != 10 & x$ITERNUM != 16 & x$ITERNUM != 82 & x$ITERNUM != 87 & x$ITERNUM != 92 & x$ITERNUM != 98 & x$ITERNUM != 103 & x$ITERNUM != 108 & x$ITERNUM != 113 & x$ITERNUM != 119 & x$ITERNUM != 43 & x$ITERNUM != 48 & x$ITERNUM != 54 & x$ITERNUM != 59 & x$ITERNUM != 64 & x$ITERNUM != 69 & x$ITERNUM != 75 & x$ITERNUM != 80)
-x_up = mean(x$METIME) + 2*sd(x$METIME)
-x_dn = mean(x$METIME) - 2*sd(x$METIME)
-binsize=50
-x = subset(x, x$METIME >= x_dn & x$METIME <= x_up)
-nbins <- ceiling((max(x$METIME)-min(x$METIME)) / binsize)
-h = hist(x$METIME, right=F, breaks=nbins,plot=F)
-xmin <-min(x$METIME)
-xmax <-max(x$METIME)
-ymax <- max(h$counts)
-ymax <- ceiling(ymax/100)*100
-ymax <- max(h$counts)
-ymax <- 40
-plot(h, freq=TRUE,ylim=c(0,ymax), xlim=c(xmin-100,xmax+300),col="blue", main='ET frequency on INC16384', 
-sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), 
-xlab='ET (ms)', ylab=expression('Frequency'))
-dev.off()
+
--- pt_characterization/new_data/empv5_pt_hist.r
+++ pt_characterization/new_data/empv5_pt_hist.r
@@ -223,8 +223,8 @@ dev.off()
 x = read.csv(file="8192_sec.dat",head=TRUE,sep="\t")
 setEPS()
 postscript("8192_sec_pt_hist1_v5.eps")
-#x <- subset(x, x$ITERNUM <= 40) 
-x <- subset(x, x$ITERNUM != 84 & x$ITERNUM != 95 & x$ITERNUM != 105 & x$ITERNUM != 116 & x$ITERNUM != 45 & x$ITERNUM != 56 & x$ITERNUM != 66 & x$ITERNUM != 77)
+x <- subset(x, x$ITERNUM <= 40) 
+#x <- subset(x, x$ITERNUM != 84 & x$ITERNUM != 95 & x$ITERNUM != 105 & x$ITERNUM != 116 & x$ITERNUM != 45 & x$ITERNUM != 56 & x$ITERNUM != 66 & x$ITERNUM != 77)
 x_up = mean(x$PRTIME) + 2*sd(x$PRTIME)
 x_dn = mean(x$PRTIME) - 2*sd(x$PRTIME)
 x = subset(x, x$PRTIME >= x_dn & x$PRTIME <= x_up)
@@ -235,7 +235,7 @@ nbins <- ceiling((xmax-xmin) / binsize)
 h = hist(x$PRTIME, right=F, breaks=nbins,plot=F)
 xmin <-xmin-17
 xmax <-xmax+15
-plot(h, ylim=c(0,15), xaxt='n',freq=TRUE,xlim=c(xmin,xmax),col="green", main='PT frequency on INC8192', 
+plot(h, ylim=c(0,10), xaxt='n',freq=TRUE,xlim=c(xmin,xmax),col="green", main='PT frequency on INC8192', 
 sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), xlab='PT (ms)', ylab=expression('Frequency'))
 xaxtl <- seq(xmin,xmax,by=binsize)
 axis(side=1, at=xaxtl, labels=xaxtl)
@@ -244,7 +244,8 @@ dev.off()
 x = read.csv(file="16384_sec.dat",head=TRUE,sep="\t")
 setEPS()
 postscript("16384_sec_pt_hist1_v5.eps")
-x <- subset(x, x$ITERNUM != 10 & x$ITERNUM != 16 & x$ITERNUM != 82 & x$ITERNUM != 87 & x$ITERNUM != 92 & x$ITERNUM != 98 & x$ITERNUM != 103 & x$ITERNUM != 108 & x$ITERNUM != 113 & x$ITERNUM != 119 & x$ITERNUM != 43 & x$ITERNUM != 48 & x$ITERNUM != 54 & x$ITERNUM != 59 & x$ITERNUM != 64 & x$ITERNUM != 69 & x$ITERNUM != 75 & x$ITERNUM != 80)
+x <- subset(x, x$ITERNUM <= 40) 
+x <- subset(x, x$ITERNUM != 10 & x$ITERNUM != 16)
 x_up = mean(x$PRTIME) + 2*sd(x$PRTIME)
 x_dn = mean(x$PRTIME) - 2*sd(x$PRTIME)
 x = subset(x, x$PRTIME >= x_dn & x$PRTIME <= x_up)
@@ -255,7 +256,8 @@ nbins <- ceiling((xmax-xmin) / binsize)+1
 h = hist(x$PRTIME, right=F, breaks=nbins,plot=F)
 xmin <-xmin-57
 xmax <-xmax+45
-plot(h, ylim=c(0,25), xaxt='n',freq=TRUE,xlim=c(xmin,xmax),col="green", main='PT frequency on INC16384', 
+ymax <- 15
+plot(h, ylim=c(0,ymax), xaxt='n',freq=TRUE,xlim=c(xmin,xmax),col="green", main='PT frequency on INC16384', 
 sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), xlab='PT (ms)', ylab=expression('Frequency'))
 xaxtl <- seq(xmin,xmax,by=binsize)
 axis(side=1, at=xaxtl, labels=xaxtl)
