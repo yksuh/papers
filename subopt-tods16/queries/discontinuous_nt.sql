1dd9b303cdddf1cd7ab1476f31a8461140082f7c
yksuh <yksuh@cs.arizona.edu>
2017-03-16 15:17:39 +0900

    discontinuous operator identification

--- subopt-tods16/queries/discontinuous_nt.sql
+++ subopt-tods16/queries/discontinuous_nt.sql
@@ -4,12 +4,12 @@
 -- labshelf 5.19/6.0
 
 -- Gather all estimated cost per operator per plan across runs
--- DiscontOp_S0: DiscontinuousOperator_Step0
-DROP TABLE DiscontOp_S0;
-CREATE TABLE DiscontOp_S0 AS 
+-- DiscontOpNt_S0: DiscontinuousOperator_Step0
+DROP TABLE DiscontOpNt_S0;
+CREATE TABLE DiscontOpNt_S0 AS 
 	SELECT 
 		--distinct 
-		er.DBMSNAME as DBMS,
+		er.DBMS,
 		er.runid, 
 		q.querynumber as querynum, 
 		qe.cardinality as card, 
@@ -25,8 +25,8 @@ CREATE TABLE DiscontOp_S0 AS
 	     AZDBLAB_QUERYEXECUTIONHASPLAN qep,
 	     AZDBLAB_PLANOPERATOR po,
 	     AZDBLAB_QUERYEXECUTIONHASSTAT qes
-	WHERE --er.runid IN (select runid from EH_Chosen_Runs_Ver1)  -- labshelves: 5.19/6.0
-	  er.runid IN (select runid from NSOExh_Runs) -- labshelf: 7.10
+	WHERE --er.runid IN (select runid from EH_Chosen_Runs_Ver1) 
+	   er.runid IN (select runid from EHNT_RT_Chosen_Runs_Ver1)
 	  --AND q.queryid = 132
 	  AND qe.ITERNUM = 1
 	  AND er.runid = q.runid 
@@ -43,15 +43,14 @@ CREATE TABLE DiscontOp_S0 AS
 	  --AND po.operatorname = 'HASH JOIN'
 	--ORDER BY runid, querynum, card asc, qep.PLANID, po.operatorname, qe.cardinality, value	
 	ORDER BY runid, querynum, card asc, PLANID, opname, oporder, statname;	
-ALTER TABLE DiscontOp_S0 ADD PRIMARY KEY (runid,querynum,card,PLANID,opID,statName);
---select * from DiscontOp_S0 where opname = 'ALL:Full Table Scan'
-column opname FORMAT A20;
+ALTER TABLE DiscontOpNt_S0 ADD PRIMARY KEY (runid,querynum,card,PLANID,opID,statName);
+--select * from DiscontOpNt_S0 where opname = 'ALL:Full Table Scan'
+--select distinct opname from DiscontOpNt_S0 where DBMS = 'oracle'column opname FORMAT A20;
 column dbms FORMAT A20;
-select distinct dbms, opname from DiscontOp_S0 order by dbms, opname;
-select distinct dbms, opname from DiscontOp_S3 order by dbms, opname;
+select distinct dbms, opname from DiscontOpNt_S0 order by dbms, opname
 
-DROP TABLE DiscontOp_S1;
-CREATE TABLE DiscontOp_S1 AS 
+DROP TABLE DiscontOpNt_S1;
+CREATE TABLE DiscontOpNt_S1 AS 
 	SELECT  t0.DBMS,
 		t0.runid,
 		t0.querynum,
@@ -64,8 +63,8 @@ CREATE TABLE DiscontOp_S1 AS
 		t0.estCost as low_cost,
 		t1.estCost as high_cost,
 		((t1.estCost-t0.estCost)/10000) as slope
-	FROM DiscontOp_S0 t0, -- low card
-	     DiscontOp_S0 t1  -- high card 
+	FROM DiscontOpNt_S0 t0, -- low card
+	     DiscontOpNt_S0 t1  -- high card 
 	WHERE t0.runid = t1.runid 
 	AND t0.querynum = t1.querynum 
 	AND (t0.card+10000 = t1.card -- adjacent cardinalities
@@ -73,15 +72,15 @@ CREATE TABLE DiscontOp_S1 AS
 	AND t0.planid = t1.planid 
 	AND t0.opID = t1.opID
 	ORDER BY runid, querynum, high_card asc, PLANID, opID, slope;	
-ALTER TABLE DiscontOp_S1 ADD PRIMARY KEY (runid,querynum,high_card,PLANID,opID,slope);
---select * from DiscontOp_S1 where opname = 'HASH' and high_card >= 1100000 and high_card <= 1200000
---select card from DiscontOp_S0 where opname = 'SELECT STATEMENT' 
---(select distinct card from DiscontOp_S0 where opname = 'SELECT STATEMENT' minus select high_card from DiscontOp_S1 where opname = 'SELECT STATEMENT')
---select high_card, slope from DiscontOp_S1 where opname = 'ALL:Full Table Scan'
+ALTER TABLE DiscontOpNt_S1 ADD PRIMARY KEY (runid,querynum,high_card,PLANID,opID,slope);
+--select * from DiscontOpNt_S1 where opname = 'HASH' and high_card >= 1100000 and high_card <= 1200000
+--select card from DiscontOpNt_S0 where opname = 'SELECT STATEMENT' 
+--(select distinct card from DiscontOpNt_S0 where opname = 'SELECT STATEMENT' minus select high_card from DiscontOpNt_S1 where opname = 'SELECT STATEMENT')
+--select high_card, slope from DiscontOpNt_S1 where opname = 'ALL:Full Table Scan'
 
 column opname FORMAT A20;
-DROP TABLE DiscontOp_S1_Stat;
-CREATE TABLE DiscontOp_S1_Stat AS 
+DROP TABLE DiscontOpNt_S1_Stat;
+CREATE TABLE DiscontOpNt_S1_Stat AS 
 	SELECT  t0.DBMS,
 		t0.runid,
 		t0.querynum,
@@ -91,14 +90,14 @@ CREATE TABLE DiscontOp_S1_Stat AS
 		t0.oporder,
 		avg(t0.slope) as slp_avg,
 		stddev(t0.slope) as slp_std
-	FROM DiscontOp_S1 t0
+	FROM DiscontOpNt_S1 t0
 	GROUP BY DBMS,runid, querynum,PLANID,opID,opname,oporder 
 	ORDER BY DBMS,runid, querynum,PLANID,oporder,opname;
-ALTER TABLE DiscontOp_S1_Stat ADD PRIMARY KEY (runid,querynum,PLANID,opID);
+ALTER TABLE DiscontOpNt_S1_Stat ADD PRIMARY KEY (runid,querynum,PLANID,opID);
 
--- select planid,opname, oporder, slp_avg,slp_std from DiscontOp_S1_Stat 
-DROP TABLE DiscontOp_S2;
-CREATE TABLE DiscontOp_S2 AS 
+-- select planid,opname, oporder, slp_avg,slp_std from DiscontOpNt_S1_Stat 
+DROP TABLE DiscontOpNt_S2;
+CREATE TABLE DiscontOpNt_S2 AS 
 	SELECT	t0.DBMS,
 		t0.runid,
 		t0.querynum,
@@ -107,36 +106,36 @@ CREATE TABLE DiscontOp_S2 AS
 		t0.opId,
 		t0.opname,
 		t0.oporder,
-	opname	t0.slope
-	FROM 	DiscontOp_S1 t0,
-		DiscontOp_S1_Stat t1
+		t0.slope
+	FROM 	DiscontOpNt_S1 t0,
+		DiscontOpNt_S1_Stat t1
 	WHERE t0.runid = t1.runid 
 	and t0.querynum = t1.querynum 
 	and t0.planid = t1.planid
 	and t0.opID = t1.opID 
 	-- locate one > one avg + one standard dev.
 	and t0.slope > t1.slp_avg+2*t1.slp_std; 
-ALTER TABLE DiscontOp_S2 ADD PRIMARY KEY (runid,querynum,high_card,PLANID,opID,slope);
+ALTER TABLE DiscontOpNt_S2 ADD PRIMARY KEY (runid,querynum,high_card,PLANID,opID,slope);
 
-DROP TABLE DiscontOp_S3_PerRun;
-CREATE TABLE DiscontOp_S3_PerRun AS 
+DROP TABLE DiscontOpNt_S3_PerRun;
+CREATE TABLE DiscontOpNt_S3_PerRun AS 
 	SELECT distinct 
 	       dbms,
 	       runid,
 	       opname
-	FROM DiscontOp_S2
+	FROM DiscontOpNt_S2
 	ORDER BY runid, opname desc;
-ALTER TABLE DiscontOp_S3_PerRun ADD PRIMARY KEY (dbms,runid,opname);
+ALTER TABLE DiscontOpNt_S3_PerRun ADD PRIMARY KEY (dbms,runid,opname);
 
-DROP TABLE DiscontOp_S3;
-CREATE TABLE DiscontOp_S3 AS 
+DROP TABLE DiscontOpNt_S3;
+CREATE TABLE DiscontOpNt_S3 AS 
 	SELECT distinct 
 	       dbms,
 	       opname
-	FROM DiscontOp_S3_PerRun
+	FROM DiscontOpNt_S3_PerRun
 	ORDER BY opname desc;
-ALTER TABLE DiscontOp_S3 ADD PRIMARY KEY (dbms,opname);
+ALTER TABLE DiscontOpNt_S3 ADD PRIMARY KEY (dbms,opname);
 column opname FORMAT A20;
 column dbms FORMAT A20;
-select * from DiscontOp_S3;
+select dbms, opname from DiscontOpNt_S3;
 
